7393ae775a0e1b35d728e53f2e6313075da71b0c
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-12-12 01:08:38 -0500

    Added Roles to Controllers

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,10 @@ defmodule Phxblog.UserController do
   use Phxblog.Web, :controller
 
   alias Phxblog.User
+  alias Phxblog.Role
+
+  plug :authorize_admin when action in [:new, :create]
+  plug :authorize_user when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     users = Repo.all(User)
@@ -9,20 +13,22 @@ defmodule Phxblog.UserController do
   end
 
   def new(conn, _params) do
+    roles = Repo.all(Role)
     changeset = User.changeset(%User{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset, roles: roles)
   end
 
   def create(conn, %{"user" => user_params}) do
+    roles = Repo.all(Role) 
     changeset = User.changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "User created successfully.")
-        |> redirect(to: user_path(conn, :show, user))
+        |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", changeset: changeset, roles: roles)
     end
   end
 
@@ -32,12 +38,14 @@ defmodule Phxblog.UserController do
   end
 
   def edit(conn, %{"id" => id}) do
+    roles = Repo.all(Role)
     user = Repo.get!(User, id)
     changeset = User.changeset(user)
-    render(conn, "edit.html", user: user, changeset: changeset)
+    render(conn, "edit.html", user: user, changeset: changeset, roles: roles)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
+    roles = Repo.all(Role)
     user = Repo.get!(User, id)
     changeset = User.changeset(user, user_params)
 
@@ -47,7 +55,7 @@ defmodule Phxblog.UserController do
         |> put_flash(:info, "User updated successfully.")
         |> redirect(to: user_path(conn, :show, user))
       {:error, changeset} ->
-        render(conn, "edit.html", user: user, changeset: changeset)
+        render(conn, "edit.html", user: user, changeset: changeset, roles: roles)
     end
   end
 
@@ -61,5 +69,29 @@ defmodule Phxblog.UserController do
     conn
     |> put_flash(:info, "User deleted successfully.")
     |> redirect(to: user_path(conn, :index))
+  end
+
+  defp authorize_user(conn, _) do
+    user = get_session(conn, :current_user)
+    if user && (Integer.to_string(user.id) == conn.params["id"] || Phxblog.RoleChecker.is_admin?(user)) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to modify that user!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+  defp authorize_admin(conn, _) do
+    user = get_session(conn, :current_user)
+    if user && Phxblog.RoleChecker.is_admin?(user) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to create new users!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
